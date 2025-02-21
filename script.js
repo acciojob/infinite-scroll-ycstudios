0e3fd53dde0b3bd2dd90d111c46fc52e15722104
Yash Chandanshive <66194004+ycstudios@users.noreply.github.com>
2025-02-21 21:49:27 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,21 @@
 //your code here!
 
+const list = document.getElementById('infi-list');
+let itemCount = 10; 
+function addItems(count) {
+    for (let i = 0; i < count; i++) {
+        itemCount++; 
+        const item = document.createElement('li');
+        item.textContent = `Item ${itemCount}`;
+        list.appendChild(item);
+    }
+}
+
+
+addItems(10);
+
+list.addEventListener('scroll', () => {
+    if (list.scrollTop + list.clientHeight >= list.scrollHeight - 1) {
+        addItems(2); 
+    }
+});
